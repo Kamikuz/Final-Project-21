05ec38edec96c761414bc27d6650691dc2198eb2
KAMIKU <kamiku319@gmail.com>
2019-12-01 03:42:03 -0500

    更新

--- currency_exchange.py
+++ currency_exchange.py
@@ -8,36 +8,85 @@ import json
 
 #def the main funciton
 def main():
+	error = 0
+
 	print("Initializing...")
-	currency_list = requests.get('https://api.shenjian.io/exchange/list?appid=01b615dcc108bd15c022160f14627541')
-	currency_list = currency_list.json()
+
+	# first exception handle
+	try:
+		currency_list_zh = requests.get('https://yxue.w3.uvm.edu/cs021/currency_list_zh.json')
+		currency_list_zh = currency_list_zh.json()
+
+		currency_list_en = requests.get('https://yxue.w3.uvm.edu/cs021/currency_list_en.json')
+		currency_list_en = currency_list_en.json()
+	except:
+		print('The api is broken or no connection, try later o(╥﹏╥)o')
+
 
 	#intro info
 	print("\nWelcome to use the currency exchange system!\n")
-	print("Here is the code of currency:")
 
-	n = 1
+	language = int(input('Please choose the Language[English --> 0 | 简体中文 --> 1]: '))
 
-	for item in currency_list['data']:
-		print(n,item['code'],item['name'],sep='|',end='\n')
-		n += 1
+	if language == 0:
+		print("\nHere is the code of currency:")
+		n = 1
+		for item in currency_list_en['list']:
+			print(n,item['code'],item['name'],sep='\t|',end='\n')
+			n += 1
 
-	#user input
-	origin_currency = input("Please enter the source currency(Code): ")
-	target_currency = input("\nPlease enter the target currency you want to convert(Code): ")
-	print("Loading(*￣︶￣)...")
+		#user input
+		origin_currency = input("Please enter the source currency(Code): ")
+		origin_currency = origin_currency.upper()
 
-	#call the request fun to get the rate
-	RATE = float(request(origin_currency,target_currency))
+		target_currency = input("\nPlease enter the target currency you want to convert(Code): ")
+		target_currency = target_currency.upper()
+	
+		print("Loading(*￣︶￣)...")
 
-	#promot the user to input the amount
-	amount = float(input("\nPlease enter the number you want to convert: "))
+		#call the request fun to get the rate
+		RATE = float(request(origin_currency,target_currency))
 
-	#calculate
-	reuslt_amount = amount * RATE
+		#promot the user to input the amount
+		amount = float(input("\nPlease enter the number you want to convert: "))
 
-	#call the output function to give the result
-	output(origin_currency,target_currency,RATE,reuslt_amount,amount)
+		#calculate
+		reuslt_amount = amount * RATE
+
+		#call the output function to give the result
+		output(origin_currency,target_currency,RATE,reuslt_amount,amount)
+
+	elif language == 1:
+		print("\n下面是货币列表及其代码:")
+		n = 1
+		for item in currency_list_zh['data']:
+			print(n,item['code'],item['name'],sep='\t|',end='\n')
+			n += 1
+
+		#user input
+		origin_currency = input("请输入原货币的代码: ")
+		origin_currency = origin_currency.upper()
+		
+		target_currency = input("\n请输入想要转换的货币代码: ")
+		target_currency = target_currency.upper()
+	
+		print("请稍等(*￣︶￣)...")
+
+		#call the request fun to get the rate
+		RATE = float(request(origin_currency,target_currency))
+
+		#promot the user to input the amount
+		amount = float(input("\n请输入转换金额: "))
+
+		#calculate
+		reuslt_amount = amount * RATE
+
+		#call the output function to give the result
+		output(origin_currency,target_currency,RATE,reuslt_amount,amount)
+
+	else:
+		error = 1
+
 
 def request(x,y):
 	#impot the appid to get the api
